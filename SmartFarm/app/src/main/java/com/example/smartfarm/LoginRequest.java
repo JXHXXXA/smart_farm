4cc0c7c0414be9e13d67351360d977cd9bd5d85e
ChoiChangYong <chanranhan_@naver.com>
2019-05-26 10:33:19 +0900

    login api url update

--- SmartFarm/app/src/main/java/com/example/smartfarm/LoginRequest.java
+++ SmartFarm/app/src/main/java/com/example/smartfarm/LoginRequest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class LoginRequest extends StringRequest {
 
-    final static private String URL = "https://uxilt2te-api.ap-northeast-2.amazonaws.com/dev/login";
+    final static private String URL = "https://uxilt2y0g6.execute-api.ap-northeast-2.amazonaws.com/dev/login";
     private Map<String, String> parameters;
 
     public LoginRequest(String id, String password, Response.Listener<String> listener){
