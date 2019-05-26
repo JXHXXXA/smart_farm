2e4e92c90d4b2b0a595f5b3cc5721aa9abbb2ee6
ChoiChangYong <chanranhan_@naver.com>
2019-05-26 12:20:56 +0900

    farm list - custom layout

---
+++ SmartFarm/app/src/main/java/com/example/smartfarm/Farm.java
@@ -0,0 +1,93 @@
+package com.example.smartfarm;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Farm extends RelativeLayout {
+    RelativeLayout layout;
+    ImageView image;
+    LinearLayout text_layout;
+    TextView name;
+    TextView location;
+    TextView total_area;
+
+    public Farm(Context context) {
+        super(context);
+        initView();
+    }
+
+    public Farm(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+        getAttrs(attrs);
+    }
+
+    public Farm(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs);
+        initView();
+        getAttrs(attrs, defStyle);
+    }
+
+    private void initView() {
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+        View v = li.inflate(R.layout.farm, this, false);
+        addView(v);
+        layout = (RelativeLayout) findViewById(R.id.farm_layout);
+        image = (ImageView) findViewById(R.id.farm_image);
+        text_layout = (LinearLayout) findViewById(R.id.farm_text_layout);
+        name = (TextView) findViewById(R.id.farm_name);
+        location = (TextView) findViewById(R.id.farm_location);
+        total_area = (TextView) findViewById(R.id.farm_total_area);
+    }
+
+    private void getAttrs(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.Farm);
+        setTypeArray(typedArray);
+    }
+
+    private void getAttrs(AttributeSet attrs, int defStyle) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.Farm, defStyle, 0);
+        setTypeArray(typedArray);
+    }
+
+    private void setTypeArray(TypedArray typedArray) {
+        int image_resID = typedArray.getResourceId(R.styleable.Farm_farm_image, R.drawable.farm1);
+        image.setImageResource(image_resID);
+
+        String name_string = typedArray.getString(R.styleable.Farm_farm_name);
+        name.setText(name_string);
+
+        String location_string = typedArray.getString(R.styleable.Farm_farm_location);
+        location.setText(location_string);
+
+        String total_area_string = typedArray.getString(R.styleable.Farm_farm_total_area);
+        total_area.setText(total_area_string);
+
+        typedArray.recycle();
+    }
+
+    void setFarmImage(int image_resID) {
+        image.setImageResource(image_resID);
+    }
+
+    void setFarmName(String name_string) {
+        name.setText(name_string);
+    }
+
+    void setFarmLocation(String location_string) {
+        location.setText(location_string);
+    }
+
+    void setFarmTotalArea(String total_area_string) {
+        total_area.setText(total_area_string);
+    }
+
+}
--- SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.java
+++ SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.java
@@ -6,25 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView farm1Img;
+    LinearLayout farm_list;
+    Farm farm;
     ImageView backImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        farm1Img = (ImageView) findViewById(R.id.farm1);
-        farm1Img.setOnClickListener(new View.OnClickListener() {        // Image를 클릭한 경우
-            public void onClick(View v) {
-                Intent intent = new Intent(
-                    getApplicationContext(), // 현재 화면의 제어권자
-                    AreaActivity.class); // 다음 넘어갈 클래스 지정
-                startActivity(intent); // 다음 화면으로 넘어간다
-            }
-        });
-
         backImg = (ImageView) findViewById(R.id.main_back);
         backImg.setOnClickListener(new View.OnClickListener() {        // Image를 클릭한 경우
             public void onClick(View v) {
@@ -34,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
+
+//        String farm_image[] = new String[3];
+//        farm_image[0] = "farm1";
+//        farm_image[1] = "farm2";
+//        farm_image[2] = "farm3";
+
+        farm_list = (LinearLayout) findViewById(R.id.farm_list);
+
+        farm = new Farm(this);
+        farm.setFarmImage(R.drawable.farm1);
+        farm.setFarmName("코끼리하마농장");
+        farm.setFarmLocation("전라남도보성군");
+        farm.setFarmTotalArea("총6동");
+        farm_list.addView(farm);
+
+        farm = new Farm(this);
+        farm.setFarmImage(R.drawable.farm2);
+        farm.setFarmName("돼지사과농장");
+        farm.setFarmLocation("경상남도진주시");
+        farm.setFarmTotalArea("총3동");
+        farm_list.addView(farm);
+
+        farm = new Farm(this);
+        farm.setFarmImage(R.drawable.farm3);
+        farm.setFarmName("커피농장");
+        farm.setFarmLocation("충청북도천안시");
+        farm.setFarmTotalArea("총2동");
+        farm_list.addView(farm);
+
     }
 }
